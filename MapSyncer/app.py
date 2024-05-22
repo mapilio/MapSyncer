b1751427ef484f9e02c7b80950f7490c32ddd753
Yusuf Esat <xyusufesat@gmail.com>
2024-05-22 09:52:25 +0300

    Code quality has been improved.

--- MapSyncer/app.py
+++ MapSyncer/app.py
@@ -5,6 +5,7 @@ import shutil
 import ssl
 import webbrowser
 
+import requests
 from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from mapilio_kit.base import authenticator
 from mapilio_kit.components.edit_config import edit_config
@@ -32,6 +33,10 @@ CERT_PEM = os.path.join(os.path.expanduser("~"), ".config", "mapilio", "configs"
 KEY_PEM = os.path.join(os.path.expanduser("~"), ".config", "mapilio", "configs", "MapSyncer",
                        "key.pem")
 
+MAPILIO_API_ENDPOINT = "https://end.mapilio.com"
+client_id = 8
+client_secret = "7TccnOQOdxnUIFgOjFiotiuFC4lWQhMeilddgxJJ"
+
 
 def get_args_mapilio(func):
     arg_names = func.__code__.co_varnames[:func.__code__.co_argcount]
@@ -54,6 +59,23 @@ def load_data():
     except Exception as e:
         print(e)
         return None
+
+
+def remove_mapilio_account():
+    MAPILIO_CONFIG_PATH = os.getenv(
+        "MAPILIO_CONFIG_PATH",
+        os.path.join(
+            os.path.expanduser("~"),
+            ".config",
+            "mapilio",
+            "configs",
+            "CLIENT_USERS",
+        ),
+    )
+    if os.path.exists(MAPILIO_CONFIG_PATH):
+        os.remove(MAPILIO_CONFIG_PATH)
+        return True
+    return False
 
 
 osm_auth = Auth(
@@ -86,6 +108,7 @@ def kartaview_login_page():
     return render_template('kartaview-login.html')
 
 
+# OpenStreetMap-Kartaview Login
 @app.route('/authenticate-kartaview', methods=['POST'])
 def authenticate_kartaview():
     login_url = osm_auth.login()
@@ -96,10 +119,11 @@ def authenticate_kartaview():
 
 @app.route('/authenticate/', methods=['GET'])
 def callback():
+    global osm_token_to_mapilio
     access_token = osm_auth.callback(request.url)
+    osm_token_to_mapilio = osm_auth.oauth.token
     parsed_data = json.loads(json.dumps(access_token))
     token = parsed_data['access_token']
-
     if access_token:
         return redirect('https://127.0.0.1:5050/get_my_data/' + token)
     else:
@@ -120,6 +144,7 @@ def check_status(sequence_id):
     return jsonify({"status": status})
 
 
+# Mapilio Login
 @app.route('/mapilio-login', methods=['POST'])
 def mapilio_login():
     args = get_args_mapilio(edit_config)
@@ -141,9 +166,24 @@ def mapilio_login():
         return render_template('mapilio-login.html', message=message)
 
 
+@app.route('/mapilio-login-with-osm', methods=['POST'])
+def mapilio_login_with_osm():
+    token = osm_token_to_mapilio['access_token']
+    resp = requests.post(f"{MAPILIO_API_ENDPOINT}/oauth-api/openstreetmap/authenticate",
+                         params={"token": token, "client_id": client_id,
+                                 "client_secret": client_secret})
+    resp_json = resp.json()
+    if resp.status_code == 200:
+        mapilio_access_token = resp_json.get('access_token')
+        return jsonify({"access_token": mapilio_access_token})
+    return jsonify({"error": "Mapilio authentication failed", "status_code": resp.status_code})
+
+
 @app.route('/display-sequence', methods=['GET'])
 def display_sequence():
-    """Displays the sequence data"""
+    """
+    The page where sequences fetched from Kartaview are shown.
+    """
     karta_auth = check_kartaview_authenticate()
     if karta_auth:
         user_name = session.get('user_name') or request.form.get('user_name')
@@ -156,8 +196,10 @@ def display_sequence():
 
     if len(list_all_users()) == 0:
         return render_template('mapilio-login.html', karta_authenticate=True)
-    elif len(list_all_users()) >= 2:
-        username = input("Found multiple Mapilio accounts. Please specify your username.\n")
+    elif len(list_all_users()) == 2:
+        status = remove_mapilio_account()
+        if status:
+            return render_template('mapilio-login.html', karta_authenticate=True)
     else:
         check_authenticate = True
 
@@ -205,23 +247,27 @@ def display_sequence():
 
 @app.route('/details', methods=['GET', 'POST'])
 def details_page():
-    karta_auth = check_kartaview_authenticate()
-
-    if not karta_auth:
-        return redirect(url_for('kartaview_login_page'))
-
-    if len(list_all_users()) == 0:
-        return render_template('mapilio-login.html')
-
+    """
+    Users can check whether the download, upload, and json operations of the sequences are successful or not.
+    """
+    sequence_filter = 'all'
     download_logs = load_data()
 
-    if download_logs is None:
-        message = "No data available. Please check the terminal for further details."
-        return render_template('details.html', message=message)
+    # GET
+    if request.method == 'GET':
+        karta_auth = check_kartaview_authenticate()
+        if not karta_auth:
+            return redirect(url_for('kartaview_login_page'))
+        if len(list_all_users()) == 0:
+            return render_template('mapilio-login.html')
+        elif len(list_all_users()) == 2:
+            status = remove_mapilio_account()
+            if status:
+                return render_template('mapilio-login.html', karta_authenticate=True)
+        if download_logs is None:
+            message = "No data available. Please check the terminal for further details."
+            return render_template('details.html', message=message)
 
-    sequence_filter = 'all'
-
-    # POST
     if request.method == 'POST':
         sequence_filter = request.form['filter']
 
@@ -240,13 +286,21 @@ def details_page():
 
 @app.route('/sequence-edit', methods=['GET', 'POST'])
 def update_json():
-    karta_auth = check_kartaview_authenticate()
-    if not karta_auth:
-        return redirect(url_for('kartaview_login_page'))
-    if len(list_all_users()) == 0:
-        return render_template('mapilio-login.html')
-
+    """
+    Changes a requested value from the log file where the download, upload, json data extraction operations
+    of the arrays are kept.
+    """
+    # GET
     if request.method == 'GET':
+        karta_auth = check_kartaview_authenticate()
+        if not karta_auth:
+            return redirect(url_for('kartaview_login_page'))
+        if len(list_all_users()) == 0:
+            return render_template('mapilio-login.html')
+        elif len(list_all_users()) == 2:
+            status = remove_mapilio_account()
+            if status:
+                return render_template('mapilio-login.html', karta_authenticate=True)
         return render_template('sequence-edit.html')
 
     # POST
@@ -272,6 +326,9 @@ def update_json():
 
 @app.route('/download-sequence', methods=['POST'])
 def download_sequence(sequence_id=None):
+    """
+    Download sequence from Kartaview
+    """
     user_name = session.get('user_name')
     response, _ = download_progress_bar()
 
@@ -286,6 +343,9 @@ def download_sequence(sequence_id=None):
 
 @app.route('/upload-sequence', methods=['POST'])
 def upload_sequence():
+    """
+    Sequence upload to Mapilio
+    """
     sequence_id = request.form.get('sequence_id')
     upload_folder_path = os.path.join(IMAGES_PATH, sequence_id)
     download_logs = load_data()
@@ -313,6 +373,9 @@ def upload_sequence():
 
 @app.route('/download-progress-bar', methods=['POST'])
 def download_progress_bar():
+    """
+    Progress bar of sequences downloaded from Kartaview
+    """
     sequence_id = request.form.get('sequence_id')
     osc_api_instance = OSCApi(OSCAPISubDomain.PRODUCTION)
     photos, error = osc_api_instance.get_photos(sequence_id)
@@ -325,6 +388,9 @@ def download_progress_bar():
 
 @app.route('/get-user-sequences', methods=['POST'])
 def get_user_sequences():
+    """
+    Retrieval of sequences from the user's Kartaview account.
+    """
     user_name = session.get('user_name') or request.form.get('user_name')
     osc_api_instance = OSCApi(OSCAPISubDomain.PRODUCTION)
     sequences, error = osc_api_instance.user_sequences(user_name)
@@ -348,6 +414,9 @@ def get_missing_sequences():
 
 @app.route('/remove-accounts', methods=['GET'])
 def remove_accounts():
+    """
+    Logs out of both Kartaview and Mapilio accounts.
+    """
     MAPILIO_CONFIG_PATH = os.getenv(
         "MAPILIO_CONFIG_PATH",
         os.path.join(
@@ -370,6 +439,7 @@ def main():
     ssl_context.load_cert_chain(CERT_PEM, KEY_PEM)
     webbrowser.open('https://127.0.0.1:5050')
     app.run(host='0.0.0.0', port=5050, threaded=True, debug=False, ssl_context=ssl_context)
+
 
 if __name__ == '__main__':
     main()
